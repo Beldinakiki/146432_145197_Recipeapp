524ca7437e31c972aa485a6a6d6f0c3e8a4d2c85
Beldinakiki <beldina.kinyua@gmail.com>
2024-01-06 03:10:18 +0300

    First commit

---
+++ app/src/main/java/com/example/foodrecipes/Meal.kt
@@ -0,0 +1,17 @@
+package com.example.foodrecipes
+
+import java.io.Serializable
+
+
+
+data class Meal(
+    val id: Int,
+    val title: String,
+    val type: String,
+    val servings: Int,
+    val difficulty: String,
+    val ingredients: String,
+    val steps: String,
+    val mealImageId: Int = 0
+) : Serializable
+
